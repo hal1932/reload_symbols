390147f490a710707aa170dfa5213926755313a5
hal1932 <yu.arai.19@gmail.com>
2017-11-25 08:32:50 +0900

    execするコードを整理

--- reload1.py
+++ reload1.py
@@ -1,21 +1,13 @@
 # encoding: utf-8
-import types
-import sys
 
 def reload1(target_module_obj):
-
-    print target_module_obj.__dict__['lib2_f1'], sys.modules['lib.libsrc2'].__dict__['lib2_f1']
     reload(target_module_obj)
-    reload(sys.modules['lib.libsrc2'])
-    # with open(target_module_obj.__file__, 'r') as f:
-    #     module_source = f.read()
-    # exec(module_source, target_module_obj.__dict__, target_module_obj.__dict__)
-    target_module_obj.__dict__['lib2_f1'] = sys.modules['lib.libsrc2'].__dict__['lib2_f1']
+    with open(target_module_obj.__file__, 'r') as f:
+        module_source = f.read()
+    exec(module_source, target_module_obj.__dict__, {})
 
 import lib
 print lib.lib2_f1
 reload1(lib)
-#import reload_symbols
-#reload_symbols.reload_symbols(lib)
 print lib.lib2_f1
 
